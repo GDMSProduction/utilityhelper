5bb4f6ffeb9d4a7ccc85d711420f43c9787e0553
Jake <zammle2009wtf@gmail.com>
2018-05-31 18:13:29 -0400

    I made multiple activities and uploaded pictures.

---
+++ app/src/main/java/com/zammle2009wtfgmail/utilityhelper/SettingScreen.java
@@ -0,0 +1,13 @@
+package com.zammle2009wtfgmail.utilityhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_screen);
+    }
+}
