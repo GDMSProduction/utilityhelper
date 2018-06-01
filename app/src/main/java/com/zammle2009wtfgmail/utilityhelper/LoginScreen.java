d212472789dff07c11f097edf256ad12b109a873
Michael Sawlani <MDSawlani@student.fullsail.edu>
2018-06-01 19:42:14 -0400

    Added design to activity_login_screen.xml

--- app/src/main/java/com/zammle2009wtfgmail/utilityhelper/LoginScreen.java
+++ app/src/main/java/com/zammle2009wtfgmail/utilityhelper/LoginScreen.java
@@ -2,6 +2,8 @@ package com.zammle2009wtfgmail.utilityhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
     }
 }
