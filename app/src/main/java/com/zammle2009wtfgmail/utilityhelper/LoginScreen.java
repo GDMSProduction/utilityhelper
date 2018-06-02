028c790f3cd4f1745c038caa1e7c93b71eb02d13
Michael Sawlani <MDSawlani@student.fullsail.edu>
2018-06-02 00:47:00 -0400

    added buttons and made login button go to the MainActivity

--- app/src/main/java/com/zammle2009wtfgmail/utilityhelper/LoginScreen.java
+++ app/src/main/java/com/zammle2009wtfgmail/utilityhelper/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.zammle2009wtfgmail.utilityhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,16 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        Button login = (Button) findViewById(R.id.LoginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeActivity = new Intent(LoginScreen.this, MainActivity.class);
+
+                startActivity(changeActivity);
+            }
+        });
 
     }
 }
