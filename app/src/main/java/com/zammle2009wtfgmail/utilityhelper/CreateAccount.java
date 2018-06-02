f03a172f03132f97f81d68f63f3ec9b613f4568f
Michael Sawlani <MDSawlani@student.fullsail.edu>
2018-06-02 15:48:57 -0400

    Added buttons, made back button go to login screen, and made the Create Account button go to login screen for now

--- app/src/main/java/com/zammle2009wtfgmail/utilityhelper/CreateAccount.java
+++ app/src/main/java/com/zammle2009wtfgmail/utilityhelper/CreateAccount.java
@@ -1,7 +1,10 @@
 package com.zammle2009wtfgmail.utilityhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+
+        Button back = (Button)  findViewById(R.id.BackButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backButton = new Intent(CreateAccount.this, LoginScreen.class);
+
+                startActivity(backButton);
+            }
+        });
+
+        Button createAccount = (Button) findViewById(R.id.CreateAccountButton);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent accountButton = new Intent(CreateAccount.this, LoginScreen.class);
+
+                startActivity(accountButton);
+            }
+        });
+
+
+
     }
 }
