e4ba67e86f49dd100d1d1b79b05c8e4b45c07487
Jake <zammle2009wtf@gmail.com>
2018-06-05 18:01:31 -0400

    Done with battery page temp

--- app/src/main/java/com/zammle2009wtfgmail/utilityhelper/MainActivity.java
+++ app/src/main/java/com/zammle2009wtfgmail/utilityhelper/MainActivity.java
@@ -1,16 +1,169 @@
 package com.zammle2009wtfgmail.utilityhelper;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+        final ImageButton pressedhistory = (ImageButton) findViewById(R.id.history);
+        pressedhistory.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+                    public void onClick(View v)
+            {
+                Intent history = new Intent (MainActivity.this, History.class );
+                startActivity(history);
+            }
+
+        });
+
+        final ImageButton pressedclean = (ImageButton) findViewById(R.id.clean);
+        pressedclean.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent history = new Intent (MainActivity.this, Something1.class );
+                startActivity(history);
+            }
+
+        });
+        final ImageButton pressedwhitelist = (ImageButton) findViewById(R.id.whitelist);
+        pressedwhitelist.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent history = new Intent (MainActivity.this, WhiteList.class );
+                startActivity(history);
+            }
+
+        });
+        final ImageButton pressednotifications = (ImageButton) findViewById(R.id.notifications);
+        pressednotifications.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent history = new Intent (MainActivity.this, Something2.class );
+                startActivity(history);
+            }
+
+        });
+        final ImageButton pressedtimer = (ImageButton) findViewById(R.id.timer);
+        pressedtimer.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent history = new Intent (MainActivity.this, CloseList.class );
+                startActivity(history);
+            }
+
+        });
+        final ImageButton pressedsettings = (ImageButton) findViewById(R.id.settings);
+        pressedhistory.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent history = new Intent (MainActivity.this, SettingScreen.class );
+                startActivity(history);
+            }
+
+        });
+
+
+
+
+
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getBaseContext().registerReceiver(null, ifilter);
+
+        // Are we charging / charged?
+        float status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+
+        // How are we charging?
+      //  int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+      //  boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+      //  boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+
+        if (isCharging == true)
+        {
+            ImageView view = (ImageView) findViewById(R.id.charge);
+
+            view.setVisibility(ImageView.VISIBLE);
+        }
+        else
+        {
+            ImageView view = (ImageView) findViewById(R.id.charge);
+
+            view.setVisibility(ImageView.INVISIBLE);
+        }
+
+
+      if (status >= 1.91f)
+      {
+          ImageView view = (ImageView) findViewById(R.id.battery5);
+
+          view.setVisibility(ImageView.VISIBLE);
+
+      }
+      else if (status >= 1.61f)
+      {
+          ImageView view = (ImageView) findViewById(R.id.battery4);
+
+          view.setVisibility(ImageView.VISIBLE);
+      }
+      else if (status >= 1.41f)
+      {
+          ImageView view = (ImageView) findViewById(R.id.battery3);
+
+          view.setVisibility(ImageView.VISIBLE);
+      }
+      else if (status >= 1.21f)
+      {
+          ImageView view = (ImageView) findViewById(R.id.battery2);
+
+          view.setVisibility(ImageView.VISIBLE);
+      }
+      else
+      {
+          ImageView view = (ImageView) findViewById(R.id.battery1);
+
+          view.setVisibility(ImageView.VISIBLE);
+      }
+
+
+
+
+
+
+    }
 
 
 
