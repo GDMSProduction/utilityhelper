73ab318c1cd9a0b8d608b9b14d6f4e7360389056
Jake <zammle2009wtf@gmail.com>
2018-06-21 16:56:07 -0400

    Added a lot

--- app/src/main/java/com/zammle2009wtfgmail/utilityhelper/WhiteList.java
+++ app/src/main/java/com/zammle2009wtfgmail/utilityhelper/WhiteList.java
@@ -17,6 +17,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 
 public class WhiteList extends AppCompatActivity {
@@ -26,6 +27,8 @@ public class WhiteList extends AppCompatActivity {
     public Button save, load;
 
     String filename = "utilityhelperstorage.txt";
+    String filename2 = "utilityhelpercloselist.txt";
+    List<templateHolder> AppInfo;
 
     static String text = "";
 
@@ -46,6 +49,130 @@ public class WhiteList extends AppCompatActivity {
         load = (Button) findViewById(R.id.buttonload);
 
 
+
+        try
+        {
+            FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+            //Toast.makeText(WhiteList.this,"Saved", Toast.LENGTH_SHORT).show();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(WhiteList.this,"Error saving file!", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        String textread = "";
+        String[] Split;
+
+        String tryread = "";
+        int tryint = 60;
+        boolean trybool = false;
+
+        try
+        {
+            FileInputStream fis = openFileInput(filename2);
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            textread = new String(buffer);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(WhiteList.this,"Error reading file!", Toast.LENGTH_SHORT).show();
+        }
+
+        Split = text.split(System.getProperty("line.separator"));
+
+        for (int i = 0; i < Split.length; ++ i)
+        {
+            if (i % 3 == 0)
+            {
+                tryread = Split[i];
+            }
+
+            else if (i % 3 == 1)
+            {
+                try {
+
+
+                    tryint = Integer.parseInt(Split[i]);
+                }
+                catch (Exception e)
+                {}
+
+            }
+
+            else if (i % 3 == 2)
+            {
+                try {
+
+
+                    trybool = Boolean.parseBoolean(Split[i]);
+                }
+                catch (Exception e)
+                {}
+
+                templateHolder hold = new templateHolder();
+
+                hold.setName(tryread);
+                hold.setInt(tryint);
+                hold.setBools(trybool);
+
+                AppInfo.add(hold);
+
+
+
+            }
+        }
+
+        String[] Split2 = text.split(System.getProperty("line.separator"));
+
+        Boolean found = false;
+
+        for (int x = 0; x < Split2.length; ++x)
+        {
+
+            for (int y = 0; y < AppInfo.size(); ++y)
+            {
+                if (Split2[x] == AppInfo.get(y).getname() )
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found == false)
+            {
+                templateHolder Adding = new templateHolder();
+                Adding.setName(Split2[x]);
+                Adding.setInt(15);
+                Boolean b = false;
+                Adding.setBools(b);
+
+                AppInfo.add(Adding);
+
+
+            }
+
+            found = false;
+
+        }
+
+
+
+        textView.setText(textread);
+
+
+
+
+
         load.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,6 +189,8 @@ public class WhiteList extends AppCompatActivity {
              //   saveFile(filename, editText.getText().toString() );
             }
         });
+
+
 
 
     }
@@ -89,7 +218,7 @@ public class WhiteList extends AppCompatActivity {
 
     public String readFile (String file)
     {
-        String text = "";
+        String textread = "";
 
         try
         {
@@ -98,7 +227,7 @@ public class WhiteList extends AppCompatActivity {
             byte[] buffer = new byte[size];
             fis.read(buffer);
             fis.close();
-            text = new String(buffer);
+            textread = new String(buffer);
         }
         catch (Exception e)
         {
@@ -106,7 +235,7 @@ public class WhiteList extends AppCompatActivity {
             Toast.makeText(WhiteList.this,"Error reading file!", Toast.LENGTH_SHORT).show();
         }
 
-        return text;
+        return textread;
     }
 
 
--- app/src/main/java/com/zammle2009wtfgmail/utilityhelper/templateHolder.java
+++ app/src/main/java/com/zammle2009wtfgmail/utilityhelper/templateHolder.java
@@ -1,4 +1,47 @@
 package com.zammle2009wtfgmail.utilityhelper;
 
-public class templateHolder {
+public class templateHolder
+
+
+
+{
+        String  name = "";
+        int Int = 60;
+        boolean Bools = false;
+
+
+
+        public String getname()
+        {
+            return name;
+        }
+
+        public int getInt()
+        {
+            return Int;
+
+        }
+
+        public boolean getbool()
+        {
+            return Bools;
+        }
+
+        public void setName(String _name)
+        {
+            name = _name;
+        }
+
+        public void setInt(int number)
+        {
+            Int = number;
+        }
+
+        public void setBools(boolean b)
+        {
+            Bools = b;
+        }
+
+
+
 }
